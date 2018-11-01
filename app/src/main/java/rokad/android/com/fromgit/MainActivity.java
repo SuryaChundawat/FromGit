a19f1de0606ca82f2e850db5287275a76e3eec73
surya <trimax.surya@gmail.com>
2018-11-01 12:36:31 +0530

    12.11.hs

--- app/src/main/java/rokad/android/com/fromgit/MainActivity.java
+++ app/src/main/java/rokad/android/com/fromgit/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //this is the frist changes from git
         //Chnages from surya
+        //this is another 
     }
 }
